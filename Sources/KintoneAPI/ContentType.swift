b2cee326af0c157e67fca97072434e7e11dd5342
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-01-21 16:29:54 +0900

    Support file uploading

---
+++ Sources/KintoneAPI/ContentType.swift
@@ -0,0 +1,20 @@
+//
+//  ContentType.swift
+//
+//
+//  Created by ky0me22 on 2025/01/21.
+//
+
+enum ContentType {
+    case applicationJSON
+    case multipartFormData(String)
+    
+    var value: String {
+        switch self {
+        case .applicationJSON:
+            "application/json"
+        case let .multipartFormData(boundary):
+            "multipart/form-data; boundary=\(boundary)"
+        }
+    }
+}
