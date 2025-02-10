fbc19bf1c9d624f6e0b59250cdd3efdd80a891f3
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-02-10 22:43:05 +0900

    Added UpdateStatusResponseTests

---
+++ Tests/KintoneAPITests/DecodableTests/UpdateStatusResponseTests.swift
@@ -0,0 +1,18 @@
+import Foundation
+import Testing
+
+@testable import KintoneAPI
+
+struct UpdateStatusResponseTests {
+    @Test
+    func response() throws {
+        let input = """
+        {
+          "revision" : "1"
+        }
+        """
+        let data = try #require(input.data(using: .utf8))
+        let actual = try JSONDecoder().decode(UpdateStatusResponse.self, from: data)
+        #expect(actual.revision == 1)
+    }
+}
