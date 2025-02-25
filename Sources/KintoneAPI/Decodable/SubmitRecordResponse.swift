e2112ff269374dcde78a89d7836885841549864c
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-02-25 11:10:41 +0900

    Make SubmitRecordResponse compliant with Equatable.

--- Sources/KintoneAPI/Decodable/SubmitRecordResponse.swift
+++ Sources/KintoneAPI/Decodable/SubmitRecordResponse.swift
@@ -5,10 +5,14 @@
 //  Created by ky0me22 on 2025/01/30.
 //
 
-public struct SubmitRecordResponse: Decodable, Sendable {
+public struct SubmitRecordResponse: Decodable, Sendable, Equatable {
     public var recordIdentity: RecordIdentity.Read
 
     public init(from decoder: any Decoder) throws {
         recordIdentity = try RecordIdentity.Read(from: decoder)
+    }
+
+    init(recordIdentity: RecordIdentity.Read) {
+        self.recordIdentity = recordIdentity
     }
 }
