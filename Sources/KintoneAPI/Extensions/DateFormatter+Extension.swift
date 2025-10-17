c76f2fc8a9e9ce43d8f3e8c5badc53553eecbfd6
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-10-17 12:15:33 +0900

    Fixed a bug where times were registered incorrectly.

--- Sources/KintoneAPI/Extensions/DateFormatter+Extension.swift
+++ Sources/KintoneAPI/Extensions/DateFormatter+Extension.swift
@@ -12,8 +12,7 @@ extension DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         formatter.calendar = Calendar(identifier: .iso8601)
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = .current
         return formatter
     }()
 
@@ -22,7 +21,7 @@ extension DateFormatter {
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.timeZone = .gmt
         return formatter
     }()
 
@@ -30,8 +29,7 @@ extension DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm"
         formatter.calendar = Calendar(identifier: .iso8601)
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = .current
         return formatter
     }()
 
